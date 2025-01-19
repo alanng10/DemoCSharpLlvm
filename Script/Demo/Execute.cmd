be5a3488f6d11b2f21a5aed34dfe9948291ea937
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-20 00:51:04 +0800

    Script

--- Script/Demo/Execute.cmd
+++ Script/Demo/Execute.cmd
@@ -1,17 +1,11 @@
 @echo off
 
-rem pushd Out\net8.0
-rem
-rem echo Status: %errorlevel%
-rem popd
-
-set DotNetInfraLibFold=.\Out\net8.0\Lib
-set InfraDeployFold=.\Out\InfraDeploy
+set LibDeployFold=.\Out\LibDeploy
 set WorkingFold=%cd%
 
 pushd Out\net8.0
 setlocal
-set "QT_PLUGIN_PATH=%WorkingFold%\%InfraDeployFold%" && set "PATH=%WorkingFold%\%InfraDeployFold%;%PATH%" && dotnet Demo.dll < ..\..\Avalon\Input.txt
+set "QT_PLUGIN_PATH=%WorkingFold%\%LibDeployFold%" && set "PATH=%WorkingFold%\%LibDeployFold%;%PATH%" && dotnet Demo.dll
 echo Status: %errorlevel%
 endlocal
 popd
