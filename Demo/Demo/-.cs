baac263bf997db4f68c3b5f055cebaefb4d6323d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-01-20 00:47:01 +0800

    Demo

---
+++ Demo/Demo/-.cs
@@ -0,0 +1,1 @@
+global using DllImportAttribute = System.Runtime.InteropServices.DllImportAttribute;
--- Demo/Demo/Extern.cs
+++ Demo/Demo/Extern.cs
@@ -2,5 +2,6 @@ namespace Demo;
 
 static class Extern
 {
-    
+    [DllImport("Lib")]
+    public static extern int Demo();
 }
